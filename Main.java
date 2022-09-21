320259d95745e69f458b13ab6e924bc9c858be2a
Fazlı <fazlgudul236@gmail.com>
2022-09-21 16:22:03 +0300

    Displaying menu and main method
    functions will be added to cases

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -9,13 +8,41 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Create a Scanner object
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter array range:");
-        int count = myObj.nextInt();
+        Scanner input = new Scanner(System.in);
+        int choice;
 
-        int[] randomNumbers = new Random().ints(count, 0, 100).toArray();
+        while (true) {
+            System.out.println("Menu driven console");
+            System.out.print("1.) the minimum of the array\n");
+            System.out.print("2.) the maximum of the array\n");
+            System.out.print("3.) the average of the array\n");
+            System.out.print("4.) the sum of elements with odd and even-numbered indexes\n");
+            System.out.print("5.) exit\n");
 
-        System.out.println(Arrays.toString(randomNumbers));
+            System.out.print("Please enter your choice\n");
+            choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("This is not a valid menu option, please try another!");
+                    break;
+            }
+        }
     }
 }
