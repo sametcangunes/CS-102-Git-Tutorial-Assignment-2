db9f287f79ba0e285a6cb02b47189602a5728369
mry01 <91725885+mry01@users.noreply.github.com>
2022-09-21 16:57:56 +0300

    min/max methods added

--- Main.java
+++ Main.java
@@ -41,6 +41,23 @@ public class Main {
         "Sum of odd numbered indexes is " + oddNumberedIndexes;
     }
 
+    public static int min(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min)
+                min = array[i];
+        }
+        return min;
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
 
